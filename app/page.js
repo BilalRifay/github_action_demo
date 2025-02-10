d8d9d20ba71b7b6b2a800200a0de50bca4aacbb7
BilalRifay <150447977+BilalRifay@users.noreply.github.com>
2025-02-10 07:14:39 +0000

    github workflow updated

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="flex items-center justify-center w-full h-screen bg-green-50 ">
+   <div className="flex items-center justify-center w-full h-screen bg-green-50 flex-col">
     <h2 className='text-red-500 bg-gray-200'>
       Hello World.............!
     </h2>
