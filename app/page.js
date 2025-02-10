eab94a2766b3e0f73f75d768f43dd272ac703571
BilalRifay <150447977+BilalRifay@users.noreply.github.com>
2025-02-10 07:10:56 +0000

    page and secret key updated

--- app/page.js
+++ app/page.js
@@ -2,10 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <h2>
+   <div className="flex items-center justify-center w-full h-screen bg-green-50 ">
+    <h2 className='text-red-500 bg-gray-200'>
       Hello World.............!
     </h2>
+    <p>
+      sdkjncskdjnskdjsk ckdcjskdjcbkd cksjdbkcsjd
+    </p>
    </div>
   );
 }
